6fc1dec094dd0bed51199f4fac3b1423ffbfdf3d
ramanguptazelhus <raman@zelhus.com>
2023-03-23 13:05:18 +0530

    Updates

---
+++ precache-manifest.5c4c6c927fc44df294b06cc1ad66ae14.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f8603cfaa25697276e06c0ee894ec",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8eaef2b165491ce9fa55",
+    "url": "/weather/static/css/main.77095a2c.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/weather/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eaef2b165491ce9fa55",
+    "url": "/weather/static/js/main.03a7630f.chunk.js"
+  },
+  {
+    "revision": "93f41cbaddd6189c9424",
+    "url": "/weather/static/js/runtime-main.b405a412.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/weather/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/weather/static/media/city.e1698332.jpg"
+  }
+]);
